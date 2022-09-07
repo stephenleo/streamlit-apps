3f5c7205db5459c3e0dd25f832db1a257d17ecd5
stephenleo <stephen.leo87@gmail.com>
2022-09-07 15:39:08 +0800

    adding tool tip

--- 01_graduate_school_admissions/streamlit_app.py
+++ 01_graduate_school_admissions/streamlit_app.py
@@ -6,8 +6,8 @@ import json
 st.title("🎓 Graduate School Admissions")
 
 # Get user inputs
-gre = st.number_input("📚 GRE Score:", step=1, max_value=800)
-gpa = st.number_input("✍️ GPA Score:", max_value=5.0)
+gre = st.number_input("📚 GRE Score:", min_value=0, max_value=800, help="GRE score in the range 0 to 800") # int max value to allow only int inputs
+gpa = st.number_input("✍️ GPA Score:", min_value=0.0, max_value=5.0, help="GPA in the range 0 to 5") # float max value to allow decimal inputs
 
 # Display the inputs
 user_input = {"gre":gre,"gpa":gpa}
@@ -15,6 +15,7 @@ st.write("User input:")
 st.write(user_input)
 
 # Code to post the user inputs to the API and get the predictions
+# Paste the URL to your GCP Cloud Run API here!
 api_url = 'https://grad-school-admission-a4rmk57awq-as.a.run.app'
 api_route = '/predict'
 
