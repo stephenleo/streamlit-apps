f6d61e7184f77f1ecf087a1798194fc94d00806a
stephenleo <stephen.leo87@gmail.com>
2023-04-23 16:39:11 +0800

    updating sg house prices

--- pages/03_🇸🇬_SG_Housing_Price_Trends.py
+++ pages/03_🇸🇬_SG_Housing_Price_Trends.py
@@ -8,6 +8,83 @@ from stqdm import stqdm
 
 url = "https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3"
 
+
+def user_inputs(unique_categories: dict) -> dict:
+    filters = {}
+    st.subheader("User Inputs")
+    st.caption("Select the filters you want to apply to the data.")
+    st.caption(
+        "Leave the dropdown selection empty to select All in that respective dropdown."
+    )
+
+    if sold_year := st.selectbox(
+        "Sold Year", options=[""] + sorted(unique_categories["sold_year"])
+    ):
+        filters["month"] = [f"{sold_year}-{month:02}" for month in range(1, 13)]
+
+    if town := st.selectbox("Town", options=[""] + sorted(unique_categories["town"])):
+        filters["town"] = town
+
+    if addresses := st.multiselect(
+        "Address", options=[""] + sorted(unique_categories["address"])
+    ):
+        split_addresses = [address.split() for address in addresses]
+        filters["block"] = [address[0] for address in split_addresses]
+        filters["street_name"] = [" ".join(address[1:]) for address in split_addresses]
+
+    if flat_type := st.selectbox(
+        "Flat Type", options=[""] + sorted(unique_categories["flat_type"])
+    ):
+        filters["flat_type"] = flat_type
+
+    if storey_range := st.selectbox(
+        "Storey Range", options=[""] + sorted(unique_categories["storey_range"])
+    ):
+        filters["storey_range"] = storey_range
+
+    if lease_commence_date := st.selectbox(
+        "Lease Commence Year",
+        options=[""] + sorted(unique_categories["lease_commence_year"]),
+    ):
+        filters["lease_commence_date"] = lease_commence_date
+
+    st.write("API Payload:")
+    st.write(filters)
+
+    return filters
+
+
+def plot_violin(
+    df: pd.DataFrame, x: str, y: str, xaxis_title: str, yaxis_title: str, title: str
+) -> None:
+    fig = px.violin(df.sort_values(x), y=y, x=x, box=True)
+    fig.update_layout(xaxis_title=xaxis_title, yaxis_title=yaxis_title, title=title)
+    st.plotly_chart(fig, use_container_width=True)
+
+
+def dashboard(df: pd.DataFrame) -> None:
+    # Price vs Floor
+    plot_violin(
+        df,
+        x="storey_range",
+        y="psf",
+        xaxis_title="Storey Range",
+        yaxis_title="PSF ($)",
+        title="Price vs Floor",
+    )
+
+    # Price vs Purchase month
+    plot_violin(
+        df,
+        x="month",
+        y="psf",
+        xaxis_title="Purchase Month",
+        yaxis_title="PSF ($)",
+        title="Price vs Purchase Month",
+    )
+
+
+##### Main #####
 st.set_page_config(
     page_title="Singapore Housing Price Trends",
     page_icon="🇸🇬",
@@ -17,6 +94,7 @@ st.set_page_config(
 st.title("🇸🇬 Singapore Housing Trends")
 st.header("Check Trends in Singapore Housing Prices")
 
+# Sidebar
 with open("descriptions/03_🇸🇬_SG_Housing_Price_Trends.md", "r") as f:
     content = f.read()
 
@@ -28,105 +106,40 @@ with st.sidebar:
 with open("data/unique_categories.json", "r") as f:
     unique_categories = json.load(f)
 
-
 # Get user inputs
-filters = {}
+filters = user_inputs(unique_categories)
 
-sold_year = st.selectbox(
-    "Sold Year", options=[""] + sorted(unique_categories["sold_year"])
-)
-if sold_year:
-    filters["month"] = [f"{sold_year}-{month:02}" for month in range(1, 13)]
-
-town = st.selectbox("Town", options=[""] + sorted(unique_categories["town"]))
-if town:
-    filters["town"] = town
-
-address = st.selectbox("Address", options=[""] + sorted(unique_categories["address"]))
-if address:
-    filters["block"] = address.split()[0]
-    filters["street_name"] = " ".join(address.split()[1:])
-
-
-flat_type = st.selectbox(
-    "Flat Type", options=[""] + sorted(unique_categories["flat_type"])
-)
-if flat_type:
-    filters["flat_type"] = flat_type
-
-
-storey_range = st.selectbox(
-    "Storey Range", options=[""] + sorted(unique_categories["storey_range"])
-)
-if storey_range:
-    filters["storey_range"] = storey_range
-
-
-lease_commence_date = st.selectbox(
-    "Lease Commence Year",
-    options=[""] + sorted(unique_categories["lease_commence_year"]),
-)
-if lease_commence_date:
-    filters["lease_commence_date"] = lease_commence_date
-
-st.write("API Payload:")
-st.write(filters)
-
-# Add a submit button
+# Submit button
 if st.button("Submit"):
     # Code to post the user inputs to the API and get the data
 
     filtered_url = f"{url}&filters={json.dumps(filters)}"
 
-    total_rows = requests.get(f"{filtered_url}&limit=1").json()["result"]["total"]
-    st.write(f"Total rows: {total_rows}")
+    try:
+        total_rows = requests.get(f"{filtered_url}&limit=1").json()["result"]["total"]
+        st.write(f"Total rows: {total_rows}")
 
-    if total_rows > 1000:
-        st.warning("Too many rows to query (max 1000). Please refine your filters.")
-    else:
-        records = []
-        step = 100
-        for start_row in stqdm(range(0, total_rows, step)):
-            offset_url = f"{filtered_url}&limit={step}&offset={start_row}"
-            records.extend(requests.get(offset_url).json()["result"]["records"])
+        if total_rows > 1000:
+            st.warning("Too many rows to query (max 1000). Please refine your filters.")
+        else:
+            records = []
+            step = 100
+            for start_row in stqdm(range(0, total_rows, step)):
+                offset_url = f"{filtered_url}&limit={step}&offset={start_row}"
+                records.extend(requests.get(offset_url).json()["result"]["records"])
 
-        df = pd.DataFrame(records)
-        st.write(df)
+            # API results to Dataframe
+            df = pd.DataFrame(records)
+            df = df.apply(pd.to_numeric, errors="ignore")
+            df["floor_area_sqft"] = df["floor_area_sqm"] * 10.764
+            df["psm"] = df["resale_price"] / df["floor_area_sqm"]
+            df["psf"] = df["resale_price"] / df["floor_area_sqft"]
+            df = df.sort_values(by="month", ascending=False)
+            # df.to_pickle("data/sg_housing_price_trends.pkl")
+            st.write(df)
 
+            # Plots
+            dashboard(df)
 
-#     # TODO
-#     api_route = "/predict"
-
-#     with st.spinner("🥁 Drumroll..."):
-#         response = requests.post(f"{api_url}{api_route}", json=[names])
-
-#     predictions_df = pd.DataFrame(response.json()["response"])
-#     predictions_df.columns = ["Name", "Boy or Girl?", "Probability"]
-#     predictions_df = predictions_df.apply(
-#         lambda x: x.str.title() if x.dtype == "object" else x
-#     )
-
-#     fig = px.bar(
-#         predictions_df,
-#         x="Probability",
-#         y="Name",
-#         color="Boy or Girl?",
-#         orientation="h",
-#         color_discrete_map={"Boy": "dodgerblue", "Girl": "lightcoral"},
-#     )
-
-#     fig.update_layout(
-#         title={"text": "Confidence in Prediction", "x": 0.5},
-#         yaxis={
-#             "categoryorder": "array",
-#             "categoryarray": predictions_df["Name"].values.tolist(),
-#             "autorange": "reversed",
-#         },
-#         xaxis={"range": [0, 1]},
-#         font={"size": 14},
-#         # width=700
-#     )
-
-#     st.write("Predictions")
-#     st.dataframe(predictions_df)
-#     st.plotly_chart(fig, use_container_width=True)
+    except Exception as e:
+        st.write(e)  # "No flats meet the filter criteria. Please adjust your filters")
